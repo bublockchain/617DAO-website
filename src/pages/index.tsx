6251df9d4555ba78c0e543524e05d5e5afd44489
Wezabis <wjorgensen29@gmail.com>
2024-09-19 12:00:11 -0400

    Fixed import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import styles from '@/styles/Home.module.scss';
+import styles from '@/src/styles/Home.module.scss';
 import { useAccount } from 'wagmi';
 import { useReadContract, useWriteContract } from 'wagmi';
 import { abi as daoABI } from '@/abis/DAO.json';
